b5d6cd836feefd658a4856ca14e3f16b038c886c
Israel Orizu <israelorizu@gmail.com>
2025-09-10 12:59:38 +0100

    added notifications.js

---
+++ src/pages/Notifications.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Notifications() {
+  return (
+    <div style={{ flex: 0.8, textAlign: 'center' }}>
+      <h1>Notifications Page</h1>
+      <p>This page is under construction. 🏗️</p>
+    </div>
+  );
+}
+
+export default Notifications;
